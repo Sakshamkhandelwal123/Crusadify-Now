4204679e9192e0bb4ea9eae11fa09a9493ea97d2
Shalini <chauhanshaliniuk@gmail.com>
2024-04-13 00:03:16 +0530

    header next structure

--- crusadify_now/crusadify_now.py
+++ crusadify_now/crusadify_now.py
@@ -31,6 +31,18 @@ def index() -> rx.Component:
               )   
             
         ),
+         rx.vstack(
+            rx.text("JOIN THE"),
+            rx.heading("Crusaders", size="8"),
+            rx.text("Explore Instant today to start building high-converting landing pages and sections, no-code required. ",style={"padding": "10px 0"}),
+            rx.button(
+                "Signup",
+                on_click=lambda: rx.redirect(docs_url),
+                size="4",
+            ),
+            align="center",
+             style={"width": "100%","padding":"84px","background-color":"#E1F2FD"}
+        ),
         style={"flex-direction":"column","width":"100%"}
         ),
 
