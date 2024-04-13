e89f6d6ffd5ca93875f592e0de5bd995db0507f5
Shalini <chauhanshaliniuk@gmail.com>
2024-04-13 22:18:27 +0530

    header

--- crusadify_now/components/header.py
+++ crusadify_now/components/header.py
@@ -1,21 +1,58 @@
 import reflex as rx
+from ..baseState import State
 
 def header()->rx.Component:
-    return rx.desktop_only(
+    menu_visible = State.menu_visible
+    print("meeee",menu_visible)
+    return rx.box(
+        rx.desktop_only(
            rx.hstack(
               rx.flex(
   rx.image(src="/logo.png", width="80px"),
                   rx.flex(
-                      rx.text("Products",style={"padding":"6px"}),
-                      rx.text("Resources",style={"padding":"6px"}),
-                      rx.text("Company",style={"padding":"6px"}),
-                      rx.text("Pricing",style={"padding":"6px"}),
-                      rx.text("Partner",style={"padding":"6px"}),
-                      rx.text("Community",style={"padding":"6px"}),
+                                                      rx.button("Login",style={"padding":"18px","width":"84px","margin":"12px","font-size":"18px"}),
+                                                        rx.button("Logout",style={"padding":"18px","width":"84px","margin":"12px","font-size":"18px"}),
+
                       
                   ),
                   style={"justify-content":"space-between","width":"100%","padding":"34px","align-items":"center"}
               )   
             
         ),
+    ),
+    rx.mobile_and_tablet(
+rx.flex(
+               rx.image(src="/hamburger.jpg", on_click=lambda:State.toggle_menu, style={"cursor": "pointer",},width="50px"),
+            rx.image(src="/logo.png", width="80px"),
+            justify="between"
+),
+
+        rx.cond(
+            menu_visible,
+rx.box(
+            rx.vstack(
+                               rx.image(src="/hamburger.jpg", on_click=lambda:State.toggle_menu, style={"cursor": "pointer",},width="50px"),
+                                rx.button("Login",style={"padding":"6px","width":"64px","margin":"24px 0px"}),    
+                          rx.button("Logout",style={"padding":"6px","width":"64px"}),    
+
+
+            ),
+             style={
+            "position": "absolute",
+            "top": "0",
+            "left": "0",
+            "width": "200px",  
+            "height": "100%",
+            "background-color": "#f0f0f0",
+            "padding": "20px",
+            "box-shadow": "2px 0 5px rgba(0, 0, 0, 0.2)",
+            "z-index": "1000"
+        }
+
+        ),
+
+        )
+            
+    
+    )
     )
