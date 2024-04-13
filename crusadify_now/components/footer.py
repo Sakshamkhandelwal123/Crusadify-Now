2133b986f290e93a32579cb91d0a1a6529be5275
Shalini <chauhanshaliniuk@gmail.com>
2024-04-13 18:54:32 +0530

    footer responsiveness

--- crusadify_now/components/footer.py
+++ crusadify_now/components/footer.py
@@ -1,20 +1,37 @@
 import reflex as rx
 
 def footer()->rx.Component:
-    return rx.flex(
-
-     
-              
-  rx.heading("Stay in touch", style={"color":"#674743","font-size":"36px"}),
+    return rx.flex(        
+  rx.heading("Stay in touch", style={"color":"#674743","font-size":"36px", "@media (max-width: 768px)": {
+                "justify-content": "center" ,
+                "padding-bottom":"20px",
+            },
+           
+            }, width=["100%","100%","50%","50%","50%"],  
+             justify_content="start",
+             align_items="center",
+             display="flex",
+             ),
                   rx.flex(
                       rx.text("Privacy policy",style={"padding":"6px"}),
                       rx.text("Product updates",style={"padding":"6px"}),
-                      rx.text("Terms of service",style={"padding":"6px"}),
+                      rx.text("Terms of service",style={"padding":"6px"}),   
+                       width=["100%","100%","50%","50%","50%"] ,
+                                 justify="end",
+                                  style={
+            "@media (max-width: 768px)": {
+                "justify-content": "center",
+                "padding-bottom":"36px",
+            },
 
-                      
+            },
+                        align_items="center",
+                        
                   ),
-                  style={"justify-content":"space-between","width":"100%","padding":"36px","align-items":"center"}
+                  style={"justify-content":"space-between","width":"100%","padding":"36px","align-items":"center"}, 
+                  flex_wrap="wrap",
+                 padding="20px 36px"
+                 
                
-            
-    
+                  
   )
