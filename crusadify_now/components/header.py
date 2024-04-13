755c539a3c3514abcf486062a7fb4d017f636138
Shalini <chauhanshaliniuk@gmail.com>
2024-04-13 16:27:05 +0530

    header component

---
+++ crusadify_now/components/header.py
@@ -0,0 +1,21 @@
+import reflex as rx
+
+def header()->rx.Component:
+    return rx.box(
+           rx.hstack(
+              rx.flex(
+  rx.image(src="/logo.png", width="80px"),
+                  rx.flex(
+                      rx.text("Products",style={"padding":"6px"}),
+                      rx.text("Resources",style={"padding":"6px"}),
+                      rx.text("Company",style={"padding":"6px"}),
+                      rx.text("Pricing",style={"padding":"6px"}),
+                      rx.text("Partner",style={"padding":"6px"}),
+                      rx.text("Community",style={"padding":"6px"}),
+                      
+                  ),
+                  style={"justify-content":"space-between","width":"100%","padding":"34px","align-items":"center"}
+              )   
+            
+        ),
+    )
