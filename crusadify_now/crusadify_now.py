95a7489cca8fcd6789343b1ed63eb6a3f73409f7
shreyam1006 <shreya.maheshwari.2000@gmail.com>
2024-04-13 12:38:26 +0530

    Add new template header

--- crusadify_now/crusadify_now.py
+++ crusadify_now/crusadify_now.py
@@ -1,6 +1,7 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 from rxconfig import config
+from crusadify_now import style
 
 import reflex as rx
 
@@ -10,7 +11,6 @@ filename = f"{config.app_name}/{config.app_name}.py"
 
 class State(rx.State):
     """The app state."""
-
 
 def index() -> rx.Component:
     return rx.center(
@@ -31,6 +31,83 @@ def index() -> rx.Component:
         height="100vh",
     )
 
+def topNav(logo, topNavItems , heroBtnTxt) -> rx.Component:
+    return rx.box(
+        rx.hstack(
+            rx.image(src=logo, width="150px"),
+            rx.hstack(
+                *[rx.link(item["label"], href=item["href"], style=style.top_nav_tab_style) for item in topNavItems],
+            ),
+            rx.button(heroBtnTxt, style=style.login_btn_style),
+            align="center",
+            justify="between",
+            width="100%",
+        ),
+        align="center",
+        spacing="4",
+        font_size="1.5em",
+        padding="10px 0px",
+        width="100%",
+        background_color="#7ACAA9",
+    )
+
+def heroSection(heroTxt, heroSubTxt, heroBtnTxt) -> rx.Component:
+    return rx.vstack(
+        rx.text(heroTxt, style=style.hero_txt_style),
+        rx.text(heroSubTxt, style=style.hero_sub_txt_style),
+        rx.button(heroBtnTxt, style=style.hero_btn_style),
+    )
+
+def heroImage(heroImg) -> rx.Component:
+    return  rx.image(src=heroImg, max_width="800px")
+    
+def header(heroTxt, heroSubTxt, heroBtnTxt) -> rx.Component:
+    return rx.box(
+        rx.hstack(
+            heroSection(heroTxt, heroSubTxt, heroBtnTxt),
+            heroImage("/template1/beige.png"),
+            align="center",
+            justify="center",
+        ),
+        align="center",
+        spacing="4",
+        font_size="1.5em",
+        padding="20px 30px",
+        width="100%",
+        background_color="#7ACAA9",
+    )
+
+def body(bodyTxt) -> rx.Component:
+    return rx.vstack(
+        rx.box(bodyTxt, size="4"),
+        align="center",
+        spacing="4",
+        font_size="1.5em",
+    )
+
+def footer(footerTxt) -> rx.Component:
+    return rx.hstack(
+        rx.box(footerTxt, size="4"),
+        align="center",
+        spacing="4",
+        font_size="1.5em",
+    )
+
+def templates() -> rx.Component:
+    topNavItems = [{"label": "Home", "href": "/"}, {"label": "About", "href": "/about"}, {"label": "Contact", "href": "/contact"}, {"label": "Shop", "href": "/shop"}, {"label": "Blog", "href": "/blog"}]
+    return rx.vstack(
+            topNav("/template1/logo.png", topNavItems, "Shop now"),
+            header("A new kind of soda.", "20+ refreshing flavours, with less sugar", "Shop now"),
+            body("This is the body"),
+            footer("Join the crusade!"),
+            align="center",
+            spacing="0",
+            font_size="1.5em",
+            width="100vw",
+     )
+    
+
 
 app = rx.App()
 app.add_page(index)
+app.add_page(templates)
---
+++ crusadify_now/style.py
@@ -0,0 +1,55 @@
+import reflex as rx
+
+hero_txt_style = dict(
+    font_weight="bold",
+    font_size="72px",
+    color=["pink", "green", "red", "purple", "black"],
+    line_height="1",
+    max_width="500px",
+)
+
+hero_sub_txt_style = dict(
+    font_size="26px",
+)
+
+hero_btn_style = dict(
+    font_size="20px",
+    background_color="black",
+    font_weight="bold",
+    color="white",
+    width="250px",
+    border_radius="24px",
+    padding="20px 40px",
+    margin="20px 0",
+    cursor="pointer",
+)
+
+top_nav_tab_style = dict(
+    font_size="20px",
+    margin="10px 20px",
+    color="black",
+    transition="0.2s",
+    _hover=dict(
+        font_weight="bold",
+        font_size="22px",
+        margin="8px 16px",
+        text_decoration="underline",
+    )
+)
+
+login_btn_style = dict(
+    font_size="20px",
+    color="white",
+    font_weight="bold",
+    border="1px solid black",
+    border_radius="24px",
+    padding="20px",
+    margin="10px 20px",
+    cursor="pointer",
+    transition="0.2s",
+    background_color="black",
+    _hover=dict(
+        background_color="#7ACAA9",
+        color="black",
+    )
+)
