19f43286072c1fd44c4d011985a5f5853901d275
Shalini <chauhanshaliniuk@gmail.com>
2024-04-14 16:50:29 +0530

    added in main file

--- crusadify_now/crusadify_now.py
+++ crusadify_now/crusadify_now.py
@@ -10,25 +10,30 @@ from .components.mainSection import mainSection
 from .components.footer import footer
 from .components.login import login
 from .components.signup import signup
+from .components.create_new import create_new
 
 docs_url = "https://reflex.dev/docs/getting-started/introduction/"
 filename = f"{config.app_name}/{config.app_name}.py"
 
 
-
 def index() -> rx.Component:
     return rx.center(
         rx.flex(
-          header(),
-        headerNext(),
-        mainSection(),
-         footer(),
-        style={"flex-direction":"column","width":"100%"}
+            header(),
+            headerNext(),
+            mainSection(),
+            footer(),
+            style={"flex-direction": "column", "width": "100%"},
         ),
-style={"font-size":"16px","font-weight":"400",}
+        style={
+            "font-size": "16px",
+            "font-weight": "400",
+        },
     )
+
 
 app = rx.App()
 app.add_page(index)
 app.add_page(login)
 app.add_page(signup)
+app.add_page(create_new)
